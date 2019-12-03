d50ad05e4b835cb50a2e73c61682402f5665b240
Zigoval <rouby.v@hotmail.com>
2019-12-03 19:48:10 +0100

    video #4

--- app.js
+++ app.js
@@ -4,17 +4,34 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+const events = [];
+
 app.use(express.json());
 app.use(
   "/graphql",
   graphqlHttp({
     schema: buildSchema(`
+      type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
       type RootQuery {
-        events: [String!]!
+        events: [Event!]!
       }
 
       type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
       }
 
       schema {
@@ -24,11 +41,19 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ["Romantic Cooking", "Sailing", "All-night Coding"];
+        return events;
       },
       createEvent: args => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
+        console.log(event);
+        events.push(event);
+        return event;
       }
     },
     graphiql: true
