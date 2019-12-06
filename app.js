e04cfd99d475a5ef6e7fdbce07cfca89f520f8ce
Zigoval <rouby.v@hotmail.com>
2019-12-06 13:19:17 +0100

    ft/ video #4

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+const Event = require("./models/event");
 
 const app = express();
 
@@ -41,23 +44,55 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              // return { ...event._doc, _id: event._doc._id.toString() };
+              return { ...event._doc };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
       },
       createEvent: args => {
-        const event = {
+        /*         const event = {
           _id: Math.random().toString(),
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
           date: args.eventInput.date
-        };
-        console.log(event);
-        events.push(event);
-        return event;
+        }; */
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(res => {
+            console.log(res);
+            return { ...res._doc };
+          })
+          .catch(e => {
+            console.log(e);
+            throw e;
+          });
       }
     },
     graphiql: true
   })
 );
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-4em4j.azure.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(res => {
+    console.log("listen on 3000");
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
