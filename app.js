4bc4e31abe231cdb1593d60549b102e55cc09c76
Zigoval <rouby.v@hotmail.com>
2019-12-07 12:23:39 +0100

    video #7

--- app.js
+++ app.js
@@ -1,86 +1,19 @@
 const express = require("express");
 const graphqlHttp = require("express-graphql");
-const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
 
-const Event = require("./models/event");
+const graphiqlSchema = require("./graphql/schema");
+const graphiqlResolvers = require("./graphql/resolvers");
 
 const app = express();
 
-const events = [];
-
 app.use(express.json());
+
 app.use(
   "/graphql",
   graphqlHttp({
-    schema: buildSchema(`
-      type Event {
-        _id: ID!
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-      }
-
-      input EventInput {
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-      }
-
-      type RootQuery {
-        events: [Event!]!
-      }
-
-      type RootMutation {
-        createEvent(eventInput: EventInput): Event
-      }
-
-      schema {
-          query: RootQuery
-          mutation: RootMutation
-      }
-    `),
-    rootValue: {
-      events: () => {
-        return Event.find()
-          .then(events => {
-            return events.map(event => {
-              // return { ...event._doc, _id: event._doc._id.toString() };
-              return { ...event._doc };
-            });
-          })
-          .catch(err => {
-            throw err;
-          });
-      },
-      createEvent: args => {
-        /*         const event = {
-          _id: Math.random().toString(),
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: args.eventInput.date
-        }; */
-        const event = new Event({
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: new Date(args.eventInput.date)
-        });
-        return event
-          .save()
-          .then(res => {
-            console.log(res);
-            return { ...res._doc };
-          })
-          .catch(e => {
-            console.log(e);
-            throw e;
-          });
-      }
-    },
+    schema: graphiqlSchema,
+    rootValue: graphiqlResolvers,
     graphiql: true
   })
 );
